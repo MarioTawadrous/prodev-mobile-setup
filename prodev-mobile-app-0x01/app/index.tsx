26e7051f5a1bd9d974cf3986847dc9498901376f
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-08-31 22:24:48 +0300

    Fix 3 Task 2

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -15,6 +15,17 @@ export default function Index() {
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
+
+    <View>
+        <Text style={styles.largeText}>
+          Typescript is great if you practice more
+        </Text>
+
+        <Text style={styles.mediumText}>
+          React Native provides you a single codebase for cross platforms
+        </Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
   );
 };
 
